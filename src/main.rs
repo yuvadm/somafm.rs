fa4f96a7fb79ec3cd53f1aa00428950a0cd27a34
Yuval Adam <_@yuv.al>
2024-05-31 12:47:11 +0200

    Use spinners for loading

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use spinners::{Spinner, Spinners};
+
 mod channels;
 
 fn main() {
+    let mut sp = Spinner::new(Spinners::Dots, "Loading SomaFM channels...".into());
     let channels = channels::get_channels();
+    sp.stop();
     let groove_salad = channels.iter().find(|c| c.id == "groovesalad").unwrap();
-    println!("{:?}", groove_salad);
+    println!("\n{:?}", groove_salad);
 }
